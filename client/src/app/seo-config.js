84694d775a2715d9bb2b9eea0b368777271b28f5
sahil-hode <sahilhode67@gmail.com>
2025-12-07 09:33:16 +0530

    last changed

--- client/src/app/seo-config.js
+++ client/src/app/seo-config.js
@@ -1,7 +1,7 @@
 // app/seo-config.js
 export const seoConfig = {
   // Base domain configuration
-  domain: 'https://shreeMahalaxmmienterprises.co.in',
+  domain: 'https://shreemahalaxmmienterprises.co.in',
   
   // Global SEO settings
   global: {
